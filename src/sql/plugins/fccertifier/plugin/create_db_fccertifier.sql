8a24fc46fabf4c136e0b2389dd4823f87b53e546
AlexandreClose <alexandre.close@gmail.com>
2017-05-10 18:31:20 +0200

    FCCERTIFY-2 : Fix a bug in sql script for creation fccertifier tables.

--- src/sql/plugins/fccertifier/plugin/create_db_fccertifier.sql
+++ src/sql/plugins/fccertifier/plugin/create_db_fccertifier.sql
@@ -3,9 +3,9 @@ CREATE TABLE fccertifier_insee_communes (
     code_commune VARCHAR( 5 ), 
     nom_commune VARCHAR( 100 ),
     PRIMARY KEY (code_commune )
-) 
+); 
 
 CREATE TABLE fccertifier_insee_pays ( 
     code_pays VARCHAR( 5 ), 
     nom_pays VARCHAR( 100 )
-) 
+); 
